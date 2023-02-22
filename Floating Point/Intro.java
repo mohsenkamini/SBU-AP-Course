2bac3ee9ab604e2723d9410939bdc0788c2e4ad0
mohsenkamini <mohsenkamini@gmail.com>
2023-02-22 11:35:04 +0330

    working with bool and i/o

---
+++ Floating Point/Intro.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Intro {
+    public static void main (String[] args) {
+
+        System.out.println(1.0-0.1-0.1-0.1-0.1-0.1 );
+
+        System.out.println(1.0-0.9 );
+    }
+}
---
+++ Floating Point/Test.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+public class Test {
+    public static void main (String[] args) {
+    
+    //int i = 123456789;
+    //// byte b = i; compile error
+    ////byte b = (byte) i;
+    //float f =i ;
+    //i = (int) f;
+    ////System.out.println( i );
+    //int m = 'a';
+    //char ch = 102 ;
+    //char letter = 'A'; // ASCII 
+    //char letter2 = '\u0041'; // unicode 16 bit
+//
+    //System.out.println( ++letter );
+    //System.out.println( letter2 );
+    //System.out.println( ch );
+
+    int num1 = (int) (Math.random() * 10);
+    int num2 = (int) (Math.random() * 10);
+
+    System.out.println( "What is " + num1 + "+" + num2 + "?:" );
+
+    
+    Scanner input = new Scanner (System.in);
+    int answer = input.nextInt();
+
+    boolean flag = num1 + num2 == answer;
+    System.out.println( "Your answer is " + flag );
+
+    }
+}
