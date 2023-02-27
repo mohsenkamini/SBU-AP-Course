8e16c455edf91867e73b78069a054d181f87f587
mohsenkamini <mohsenkamini@gmail.com>
2023-02-27 16:30:55 +0330

    overloading

--- Jalase5.java
+++ Jalase5.java
@@ -17,11 +17,43 @@ public class Jalase5 {
     // Methods' signature
     // max (int num1,int num2)
 
+    // Methods' overloading:
+    public static void max (int n1,int n2 ) {
+        int result=0;
+        if (n1 > n2)
+            result=n1;
+        else    
+            result=n2;
+        System.out.println(result);
+    }
+
+    public static void max (double n1,double n2 ) {
+        double result=0;
+        if (n1 > n2)
+            result=n1;
+        else    
+            result=n2;
+        System.out.println(result);
+    }
+
+    public static void max (int n1,int n2,int n3 ) {
+        int result=0;
+        if (n1 > n2 && n1 > n3)
+            result=n1;
+        else if (n2 > n3)
+            result=n2;
+        else
+            result=n3;
+        System.out.println(result);
+    }
 
     public static void main (String args[]) {
         Scanner input = new Scanner (System.in);
-        int num1=0,num2=2;
-        System.out.println(sign (6));
+        //int num1=0,num2=2;
+        //System.out.println(sign (6));
+        max (5,4);
+        max (10.2,11);
+        max (34,23,123);
         /*
         int i=0;
         float result=10000;
