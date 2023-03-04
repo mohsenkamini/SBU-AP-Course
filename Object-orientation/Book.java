c08bd50968e1b1e81ce8e9c9ed084e9566831b61
mohsenkamini <mohsenkamini@gmail.com>
2023-03-04 11:36:31 +0330

    static variables in class

--- Object-orientation/Book.java
+++ Object-orientation/Book.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 import java.util.Date;
 
 public class Book {
+    static int counter; // counter will be shared among all objects. This won't be in heap
     private int ISBN;
     Book (int i) {
         ISBN=i;
+        counter++;
     }
     void display() {
         System.out.println("The ISBN is: " + ISBN);
@@ -16,11 +18,13 @@ public class Book {
 class testBook {
     public static void main () {
         Book b1 = new Book(1769);
+        Book b2 = new Book(1769);
         Book b3=b1;
         b3.display();
 
         Date D = new Date();
         System.out.println(D.toString());
         System.out.println(b1);
+        System.out.println(b3.counter);
     }    
 }
