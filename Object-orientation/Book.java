1b31405467fc4795c5fc9e056e83084c7230e461
mohsenkamini <mohsenkamini@gmail.com>
2023-03-06 15:34:33 +0330

    array intro

--- Object-orientation/Book.java
+++ Object-orientation/Book.java
@@ -18,13 +18,31 @@ public class Book {
 class testBook {
     public static void main () {
         Book b1 = new Book(1769);
+        Scanner input = new Scanner (System.in);
+        
+        //Book b5 = new Book(); java only adds a no-arg constructor when there are no constructors defined by you.
         Book b2 = new Book(1769);
         Book b3=b1;
-        b3.display();
+        //b3.display();
 
         Date D = new Date();
-        System.out.println(D.toString());
-        System.out.println(b1);
-        System.out.println(b3.counter);
+        //System.out.println(D.toString());
+        //System.out.println(b1);
+        //System.out.println(b3.counter);
+
+        int x = input.nextInt();
+        int min=101,max=0,sum=0;
+        int[] array1 = new int [x];
+        for (int i=0 ; i < array1.length ; i++) 
+        {
+            array1[i] = (int) (Math.random()*101);
+            if (array1[i] > max)
+                max=array1[i];
+            if (array1[i] < min)
+                min=array1[i];
+            sum+=array1[i];
+            System.out.println(array1[i]);
+        }
+        System.out.println("max: " + max + " min: "+ min + " avg: "+ (double)(sum/array1.length));
     }    
 }
